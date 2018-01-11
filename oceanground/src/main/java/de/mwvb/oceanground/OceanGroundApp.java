38c6fb9ab5415b020a1610efe1989d673dec0c06
Marcus Warm <warm@mwvb.de>
2018-01-11 16:18:14 +0100

    0.6.5

--- oceanground/src/main/java/de/mwvb/oceanground/OceanGroundApp.java
+++ oceanground/src/main/java/de/mwvb/oceanground/OceanGroundApp.java
@@ -34,7 +34,7 @@ import de.mwvb.oceanground.model.Container;
 import spark.Request;
 
 public class OceanGroundApp extends AbstractWebApp {
-	public static final String VERSION = "0.6.4";
+	public static final String VERSION = "0.6.5";
 	// 0.2: Dependencies update, Java 8u121
 	// 0.2.1: LogConfig, always with pull
 	// 0.3: Umstellung auf plutoweb, Thymeleaf -> Velocity
@@ -59,6 +59,7 @@ public class OceanGroundApp extends AbstractWebApp {
 	// 0.6.2: MaxMemory, Restart Count, Umordnung der Container Details Felder
 	// 0.6.3: Verbesserungen bzgl. Memory Thema
 	// 0.6.4: Verbesserungen bzgl. Memory Thema
+	// 0.6.5: Verbesserungen bzgl. Memory Thema
 	public static final String TITLE = "OceanGround";
 	public static AbstractDocker docker;
 
